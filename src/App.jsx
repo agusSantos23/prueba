07efa3061b61e587f8f82ecaa98addc7c2be3aab
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-03-27 18:56:13 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  const increment = () => setCounter(counter + 1)
+  const desincrement = () => setCounter(counter - 1)
+  const setZero = () => setCounter(0)
+
+  return (
+    <>
+      <Display counter = {counter} />
+      <Button onClick = {increment}  text = {"sum"}/>
+      <Button onClick = {desincrement} text = {"rest"}/>
+      <Button onClick = {setZero}  text = {"reset"}/>
+    </>
+  )
+}
+
+
+const Display = ({counter}) => <> <h1>{counter}</h1> </>
+
+const Button = ({onClick, text}) =>{
+
+  return(
+    <>
+      <button onClick={onClick}>
+        {text}
+      </button>
+    </>
+  )
+}
+
+export default App

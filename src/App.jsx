4ef979cf4d51eb6972d6444ee9b9db846513aa4d
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-03-27 19:55:55 +0100

    Left and Right 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
 
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total,setTotal] = useState(0)
 
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
 
-  const handLeftClick = () =>{
-    const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right
-    }
-    setClicks(newClicks)
+  const handLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
-  const handRightClick = () =>{
-    const newClicks = {
-      left: clicks.left,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
+  const handRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
   }
 
   return(
     <>
-      {clicks.left}
-      <button onClick={handLeftClick}>
-        left
-      </button>
+      {left}
+      <button onClick={handLeftClick}>left</button>
+      <button onClick={handRightClick}>right</button>
+      {right}
 
-      <button onClick={handRightClick}>
-        right
-      </button>
-      {clicks.right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total = {total}</p>
     </>
   )
   

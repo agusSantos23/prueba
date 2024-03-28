bea53da6994b1d2033569632b94111616b038f16
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-03-28 11:41:25 +0100

    Forma correcta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <>{props.value}</>
+
 const App = () => {
   const [value, setValue] = useState(10)
 
@@ -9,18 +17,12 @@ const App = () => {
   }
 
   return (
-    <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
-    </div>
+    <>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </>
   )
 }
 

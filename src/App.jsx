4205cf9ec9f132d70a91127e802c5db8c207ca32
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-03-29 21:07:05 +0100

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import Note from "./components/Note"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
-const App = ({notes}) => {
-  
-  
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  console.log('render', notes.length, 'notes')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook,[])
+
+
+
   return (
     <div>
       <h1>Notes</h1>
